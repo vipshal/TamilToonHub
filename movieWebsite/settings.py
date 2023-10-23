696c242f51bfd1d8038881239d128cb64d8f7983
vipshal <vipshalgang@gmail.com>
2023-10-23 15:23:13 +0530

    whitenoise add

--- movieWebsite/settings.py
+++ movieWebsite/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 DEBUG = env("DJANGO_DEBUG")
 
 ALLOWED_HOSTS = ["localhost","127.0.0.1:8000","tamiltoonhub.up.railway.app"]
-CSRF_TRUSTED_ORIGINS = ['tamiltoonhub.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://tamiltoonhub.up.railway.app']
 
 
 # Application definition
