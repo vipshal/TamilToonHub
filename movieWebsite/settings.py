b339991c555e54e2048fff5fd861f2e7b1cbb897
vipshal <vipshalgang@gmail.com>
2023-10-23 22:32:26 +0530

    Design Modified

--- movieWebsite/settings.py
+++ movieWebsite/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DJANGO_DEBUG")
 
-ALLOWED_HOSTS = ["localhost","127.0.0.1:8000","tamiltoonhub.up.railway.app"]
+ALLOWED_HOSTS = ["localhost","127.0.0.1","tamiltoonhub.up.railway.app"]
 CSRF_TRUSTED_ORIGINS = ['https://tamiltoonhub.up.railway.app']
 
 
