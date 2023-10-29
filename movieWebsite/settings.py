23b9b4a3171140accfa0bc1f74d553e2f40d36db
vipshal <vipshalgang@gmail.com>
2023-10-29 23:19:48 +0530

    bug fix

--- movieWebsite/settings.py
+++ movieWebsite/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["localhost","127.0.0.1","tamiltoonhub.up.railway.app"]
 CSRF_TRUSTED_ORIGINS = ['https://tamiltoonhub.up.railway.app']
