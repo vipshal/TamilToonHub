e8507623f34d7a0041b8c7954bbd73c82a96ee23
vipshal <vipshalgang@gmail.com>
2023-11-05 11:30:27 +0530

    aws removed

--- movieWebsite/settings.py
+++ movieWebsite/settings.py
@@ -136,12 +136,12 @@ STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
 
 DEFAULT_AUTO_FIELD='django.db.models.AutoField'
 
-AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY")
-AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_KEY")
-AWS_STORAGE_BUCKET_NAME = 'tamiltoonhub01'
-AWS_S3_SIGNATURE_NAME = 's3v4',
-AWS_S3_REGION_NAME = 'eu-north-1'
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL =  None
-AWS_S3_VERITY = True
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY")
+# AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_KEY")
+# AWS_STORAGE_BUCKET_NAME = 'tamiltoonhub01'
+# AWS_S3_SIGNATURE_NAME = 's3v4',
+# AWS_S3_REGION_NAME = 'eu-north-1'
+# AWS_S3_FILE_OVERWRITE = False
+# AWS_DEFAULT_ACL =  None
+# AWS_S3_VERITY = True
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
