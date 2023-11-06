045a4cf3d091f1b5fca9adf8b9d438f37c4dd2bf
vipshal <vipshalgang@gmail.com>
2023-11-06 18:03:29 +0530

    Bug Fix

--- movieWebsite/settings.py
+++ movieWebsite/settings.py
@@ -132,8 +132,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR,"media")
+# MEDIA_URL = "/media/"
+# MEDIA_ROOT = os.path.join(BASE_DIR,"media")
 STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
 
 DEFAULT_AUTO_FIELD='django.db.models.AutoField'
